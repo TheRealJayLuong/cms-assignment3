cca8207aa1f6ceeb47c43dfe087d368cd48532b7
Jay <quocphong.luong@my.jcu.edu.au>
2021-01-17 13:41:24 +1000

    Plugin information

---
+++ Jay_book_review_Plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Book Review
+Plugin URI: https://bookreviews.com
+Description: create a book review post type
+Version: 1.0.0
+Author: Jay Luong
+Author URI: 
+license: GPL2
+*/
